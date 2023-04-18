8c225a565bac3a59c09a47e59c9d4bd414dcf915
Абсатар уулу Мыктыбек <smtx.muktubek@gmail.com>
2023-04-18 23:25:52 +0600

    Initial commit

---
+++ TaskProcessor/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace TaskProcessor;
+using System.Configuration;
+
+public class Program
+{
+    public static void Main(string [] args)
+    {   
+        
+    }
+
+    #region GetInstance
+
+    public static object GetInstance(string? className)
+    {
+        var classess = ConfigurationManager.AppSettings.AllKeys.ToList();
+        string objectToInstantiate = $"TaskProcessor.Processors.{className}, TaskProcessor";
+        var objectType = Type.GetType(objectToInstantiate);
+        var instantiatedObject = Activator.CreateInstance(objectType);
+        return instantiatedObject;
+    }
+
+    #endregion
+}
