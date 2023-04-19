a012f25ba1971c04609a5096512a14195c8021ca
Абсатар уулу Мыктыбек <smtx.muktubek@gmail.com>
2023-04-19 23:19:32 +0600

    Added Email sender processor

---
+++ TaskProcessor/Processors/EmailSenderProcessor.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskProcessor.Processors
+{
+    public class EmailSenderProcessor : IProcessor
+    {
+        public void Start()
+        {
+            using (StreamWriter sw = new StreamWriter($@"D:\{this.GetType().Name}.txt", true))
+            {
+                sw.WriteLine($"Start {this.GetType().Name} at {DateTime.Now} and send message: {Guid.NewGuid()}");
+            }
+        }
+    }
+}
