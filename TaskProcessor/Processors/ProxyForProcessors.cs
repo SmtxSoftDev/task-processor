13d63c8880c3e5cc4f6ab524e7412df8cd352eb5
Абсатар уулу Мыктыбек <smtx.muktubek@gmail.com>
2023-04-20 21:24:18 +0600

    Added proxy class for control over other processors

---
+++ TaskProcessor/Processors/ProxyForProcessors.cs
@@ -0,0 +1,36 @@
+﻿namespace TaskProcessor.Processors
+{
+    public class ProxyForProcessors : IProcessor
+    {
+        private IProcessor processor;
+
+        public ProxyForProcessors(IProcessor processor)
+        {
+            this.processor = processor;
+        }
+
+        public void Start()
+        {
+            try
+            {
+                if (processor == null)
+                    throw new ArgumentNullException("Processor not initialized.");
+                processor.Start();
+                string data = $"Processor: {processor.GetType().Name} start at {DateTime.Now}";
+                WriteToFile(data);
+            }
+            catch(Exception ex)
+            {
+                WriteToFile($"{ex.GetType()} {ex?.Message}");
+            }
+        }
+
+        private void WriteToFile(string data)
+        {
+            using (StreamWriter sw = new StreamWriter(@$"C:\{this.GetType().Name}.txt", true))
+            {
+                sw.WriteLine(data);
+            }
+        }
+    }
+}
